36df052363a27793fe1988a8b402cef37bb839d2
Aroon Kumar <perchaniarun@gmail.com>
2023-02-20 18:05:51 +0500

    intialization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Signup from './components/signup'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Signup />
+      </div>
+    );
+  }
+}
+ 
+export default App;
