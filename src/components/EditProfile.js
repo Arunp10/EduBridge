790ff75e244dfcd1997b1059c7be80df460b4b62
Aroon Kumar <perchaniarun@gmail.com>
2023-04-09 00:20:22 +0500

    Edit Profile Changes Updated

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -3,16 +3,16 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Avatar from "@mui/material/Avatar";
 import CssBaseline from "@mui/material/CssBaseline";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Sidebar from "./SideBar";
-import { spacing } from "@mui/system";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import { Stack } from "@mui/material";
 
 const theme = createTheme();
 
@@ -23,6 +23,7 @@ const EditProfile = () => {
   const [work, setWork] = useState("Student");
   const [domain, setDomain] = useState("Web and ML");
   const [experience, setExperience] = useState("1 Year");
+  const [profilePicture, setProfilePicture] = useState(null);
   const [isDisabled, setIsDisabled] = useState(true);
 
   const handleSubmit = (event) => {
@@ -35,6 +36,9 @@ const EditProfile = () => {
   const handleSaveProfile = () => {
     setIsDisabled(true);
   };
+  function handleProfilePictureChange(event) {
+    setProfilePicture(event.target.files[0]);
+  }
   return (
     <>
       <Sidebar />
@@ -50,7 +54,7 @@ const EditProfile = () => {
             }}
           >
             <Avatar sx={{ m: 1, bgcolor: "#47a4f2" }}>
-              <LockOutlinedIcon />
+              <BorderColorOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               Edit Profile
@@ -139,27 +143,64 @@ const EditProfile = () => {
                       disabled={isDisabled}
                     />
                   </Grid>
+                  <Box
+                    xs={12}
+                    m={1}
+                    display="flex"
+                    direction="row"
+                    alignItems="center"
+                    spacing={2}
+                    justifyContent="space-between"
+                  >
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      size="small"
+                      component="label"
+                      disabled={isDisabled}
+                    >
+                      Upload
+                      <input hidden accept="image/*" multiple type="file" />
+                    </Button>
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="label"
+                      disabled={isDisabled}
+                    >
+                      <input hidden accept="image/*" type="file" />
+                      <PhotoCamera />
+                    </IconButton>
+                  </Box>
+                  <Box
+                    xs={12}
+                    m={0}
+                    display="flex"
+                    justifyContent="space-between"
+                    alignItems="flex-start"
+                  >
+                    <Button
+                      type="button"
+                      variant="contained"
+                      color="primary"
+                      margin="normal"
+                      onClick={handleEditProfile}
+                    >
+                      Edit Profile
+                    </Button>
+                    <></>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      margin="normal"
+                      onClick={handleSaveProfile}
+                    >
+                      Save
+                    </Button>
+                  </Box>
                 </Grid>
               </Grid>
-              <Button
-                type="button"
-                variant="contained"
-                color="primary"
-                margin="normal"
-                onClick={handleEditProfile}
-              >
-                Edit Profile
-              </Button>
-              <> </>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                margin="normal"
-                onClick={handleSaveProfile}
-              >
-                Save
-              </Button>
             </Box>
           </Box>
         </Container>
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,29 +1,36 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-// import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Login from "./Login"
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import InputRoundedIcon from '@mui/icons-material/InputRounded';
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from "react-router-dom";
 
 const theme = createTheme();
+const occupations = [
+  {
+    value: "Student",
+    label: "Student",
+  },
+  {
+    value: "Professional",
+    label: "Professional",
+  }
+];
 
 export default function Signup() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
@@ -34,18 +41,23 @@ export default function Signup() {
         <Box
           sx={{
             marginTop: 2,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: '#47a4f2' }}>
-            <LockOutlinedIcon />
+          <Avatar sx={{ m: 1, bgcolor: "#47a4f2" }}>
+            <InputRoundedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={1}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -91,34 +103,49 @@ export default function Signup() {
               </Grid>
               <Grid item xs={12}>
                 <TextField
-                required
-                fullWidth
-                name="work"
-                label="Work"
-                type="work"
-                id="work"
+                  required
+                  fullWidth
+                  name="work"
+                  label="Work"
+                  type="work"
+                  id="work"
                 />
-                </Grid>
-                <Grid item xs={12}>
+              </Grid>
+              <Grid item xs={12}>
                 <TextField
-                    required
-                    fullWidth
-                    name="domain"
-                    label="Domain"
-                    type="domain"
-                    id="domain"
-                    />
-                </Grid>
-                <Grid item xs={12}>
+                  required
+                  fullWidth
+                  name="domain"
+                  label="Domain"
+                  type="domain"
+                  id="domain"
+                />
+              </Grid>
+              <Grid item xs={12}>
                 <TextField
-                    required
-                    fullWidth
-                    name="experience"
-                    label="Experience"
-                    type="experience"
-                    id="experience"
-                    />
-                </Grid>
+                  required
+                  fullWidth
+                  name="experience"
+                  label="Experience"
+                  type="experience"
+                  id="experience"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="occupation"
+                  select
+                  label="Select Occupation"
+                  defaultValue="Student"
+                  fullWidth
+                >
+                  {occupations.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
             </Grid>
             <Button
               type="submit"
