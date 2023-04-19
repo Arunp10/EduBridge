207fb21d2c2db89a4d22b7daf41bab713f6d2fcc
Aroon Kumar <perchaniarun@gmail.com>
2023-04-20 01:44:50 +0500

    EditProfileAddOnFeatures

---
+++ src/components/EditProfileSections/PersonalDetailsSection.js
@@ -0,0 +1,62 @@
+import { Grid, TextField } from "@mui/material";
+import React from "react";
+export default function PersonalDetailsSection() {
+  return (
+    <Box
+      sx={{
+        marginTop: 0,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Box component="form" noValidate sx={{ mt: 0 }}>
+        <Typography variant="overline" textAlign={"center"}>
+          Personal Details
+        </Typography>
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="firstName"
+              label="First Name"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="lastName"
+              label="Last Name"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="email"
+              label="Email"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="phone"
+              label="Phone No"
+              type="number"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+}
