a9651b8fc8975790e0fafdb259b44df1afa8e410
Aroon Kumar <perchaniarun@hotmail.com>
2023-07-15 03:32:58 +0500

    PostFundingCRUDUpdated

--- backend/routes/Funding.js
+++ backend/routes/Funding.js
@@ -23,11 +23,13 @@ const storage = multer.diskStorage({
 //Router 1: API to Post funding by Professional:
 router.post('/post',fetchUser,upload.single('file'),async(req,res)=>{
     const {description,visibility,link} = req.body;
-    const  fileName  = req.file.filename;
+    let fileName = null; 
+    if (req.file) {
+      fileName = req.file.filename; 
+    }
     try {
         const postFunding = await Funding({
             postBy: req.user.id,
-            // title: title,
             description : description,
             visibility : visibility,
             link: link,
@@ -41,6 +43,8 @@ router.post('/post',fetchUser,upload.single('file'),async(req,res)=>{
         res.status(500).json({"Error": "Some Inernal Error in API"})
     }
 })
+
+//userfetch
 
 router.get('/fetch',fetchUser,async(req,res)=>{
 
@@ -59,6 +63,7 @@ router.get('/fetch',fetchUser,async(req,res)=>{
   const existingRequest = await Connections.findOne({
       user: req.user.id,
       supervisor: { $in: postByArray },
+      status:"approved"
   });
 
     try{
@@ -66,13 +71,12 @@ router.get('/fetch',fetchUser,async(req,res)=>{
           const connectionFunding = await Funding.find({
           postBy: existingRequest.supervisor}).populate({
             path:'postBy',
-            select: 'firstName lastName occupation image' 
+            select: 'firstName lastName occupation image',
           });
         return res.status(200).json(connectionFunding);
         
-      }else{
-        return res.status(200).json(Everyfunding);
       }
+      return res.status(200).json(Everyfunding);
             
     } catch (error) {
         console.error({"Error" : error})
@@ -80,5 +84,80 @@ router.get('/fetch',fetchUser,async(req,res)=>{
     }
 
 })
+
+//supervisorFetch
+
+router.get('/supervisorfundingsfetch', fetchUser, async (req, res) => {
+  const { id: postBy } = req.user;
+  try {
+    const funding = await Funding.find({ postBy });
+    res.status(200).json(funding);
+  } catch (error) {
+    console.error({"Error" : error});
+    res.status(500).json({"Error": "Server Internal Error"});
+  }
+});
+
+router.delete('/deleteFunding/:fundingId', fetchUser, async (req, res) => {
+  try {
+    const fundingId = req.params.fundingId;
+    const userId = req.postBy
+
+    // Check if the funding entry exists
+    const fundingEntry = await Funding.findOne({ _id: fundingId, user: userId });
+
+    if (fundingEntry !== null) {
+      // The funding entry exists, so delete it
+      await Funding.deleteOne({ _id: fundingId, user: userId });
+      return res.json({ message: 'Funding entry deleted successfully' });
+    } else {
+      // The funding entry does not exist, so return a 404 error
+      return res.status(404).json({ error: 'Funding entry not found' });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.put('/updateFunding/:id',async (req, res) => {
+  const { id } = req.params;
+  const updatedData = req.body;
+  let fileName = null;
+
+  if (req.file) {
+    fileName = req.file.filename;
+  }
+  try {
+    // Find the funding entry by ID
+    const updatedFunding = await Funding.findByIdAndUpdate(id, updatedData, {
+      new: true,
+    });
+    if (!updatedFunding) {
+      return res.status(404).json({ error: "Funding entry not found" });
+    }
+
+    // Send the updated funding entry as the response
+    res.status(200).json(updatedFunding);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while updating funding' });
+  }
+});
+// Route for handling file uploads
+router.post('/uploadFile', upload.single('file'), (req, res) => {
+  try {
+    // Check if a file was uploaded
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    // File uploaded successfully, return the filename
+    res.status(200).json({ success: true, filename: req.file.filename });
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    res.status(500).json({ success: false, error: 'Failed to upload file' });
+  }
+});
+
 
 module.exports = router;
