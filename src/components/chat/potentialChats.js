334d50aa23d5d40a2abc924e6814beb435f42b8f
Your Name <hineshkumar46@gmail.com>
2023-07-23 20:17:39 +0500

    Feature: Perform ive chat

--- src/components/chat/potentialChats.js
+++ src/components/chat/potentialChats.js
@@ -1,22 +1,33 @@
-import React, { useContext } from 'react'
-import { ChatContext } from '../context/chatContext'
+import React, { useContext } from "react";
+import { ChatContext } from "../context/chatContext";
 
 function PotentialChats(props) {
-    const {potentialChats,createChat} = useContext(ChatContext);
-    const {user} = props;
+  const { potentialChats, createChat, onlineUsers } = useContext(ChatContext);
+  const { user } = props;
 
   return (
-    <div className='all-users'>
-    {potentialChats && potentialChats.map((u,index)=>{
-        return(
-            <div className='single-user' key={index} onClick={()=> createChat(user._id,u._id)}>
-                {u.firstName} {" "}{u.lastName}
-            <span className='user-online'></span>
+    <div className="all-users">
+      {potentialChats &&
+        potentialChats.map((u, index) => {
+          return (
+            <div
+              className="single-user"
+              key={index}
+              onClick={() => createChat(user._id, u._id)}
+            >
+              {u.firstName} {u.lastName}
+              <span
+                className={
+                  onlineUsers?.some((user) => user?.userId === u?._id)
+                    ? "user-online"
+                    : ""
+                }
+              ></span>
             </div>
-        );
-    })}
+          );
+        })}
     </div>
-  )
+  );
 }
 
 export default PotentialChats;
--- src/components/chat/userChats.js
+++ src/components/chat/userChats.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { useFetchRecipientUser } from '../hooks/useFetchRecipient'
 import Container from 'react-bootstrap/Container';
-import Stack from 'react-bootstrap/Stack' 
+import Stack from 'react-bootstrap/Stack';
+import { Avatar} from '@mui/material'; 
+import { ChatContext } from "../context/chatContext";
 
 function UserChats({chat,user}) {
     const {RecipientUser} = useFetchRecipientUser(chat , user);
+    const {onlineUsers} = useContext(ChatContext);
+    const isOnline = onlineUsers?.some((user) => user?.userId === RecipientUser?._id);
+
   return (
     <Stack direction="horizontal" gap={3} className='user-card align-items-cener p-2 justify-content-between'
      role='button'>
         <div className='d-flex'>
         <div className='m-2'>
-            <img src={`http://localhost:8080/${RecipientUser?.image}`} height="40px" />
+            <Avatar src={`http://localhost:8080/${RecipientUser?.image}`} height="50px" />
             </div>
             <div className='text-content'>
                 <div className="name">{RecipientUser?.firstName} {" "} {RecipientUser?.lastName}</div>
@@ -20,7 +25,7 @@ function UserChats({chat,user}) {
         <div className='d-flex flex-column align-items-end'>
             <div className='date'>12/12/2022</div>
             <div className='this-user-notifications'>2</div>
-            <span className='user-online'></span>
+            <span className={isOnline ? 'user-online' : ""}></span>
         </div>
     </Stack>
   )
